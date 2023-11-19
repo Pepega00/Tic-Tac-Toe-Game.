2a6935388a03b089de4bd0bb720bafe94e1e1a8b
FergeSS <97806343+FergeSS@users.noreply.github.com>
2023-11-19 15:31:30 +0300

    Create client.h

---
+++ client.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <future>
+#include <ctime>
+
+int time_for_move = 0; //Время на ход
+int port; //Порт
+bool gamer = true; //Состояние текущего хода
+bool move; //Состояние хода определенного игрока
+std::ofstream log("log_clients.txt", std::ios::app); //Файл с логами
+
+//Игровое поле
+char game_map[3][3] = {{' ', ' ', ' '},
+                       {' ', ' ', ' '},
+                       {' ', ' ', ' '}};
